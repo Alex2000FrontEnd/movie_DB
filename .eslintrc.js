5377518a0b579776722d925c393d928ab90f9b1c
Alex <email@gmail.com>
2024-09-11 17:47:40 +0700

    проект готов к работе

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	'overrides': [
+		{
+			'env': {
+				'browser': true,
+				'es2021': true
+			},
+			'parserOptions': {
+				'ecmaVersion': 'latest',
+				'sourceType': 'module'
+			},
+			'extends': 'eslint:recommended',
+			'files': [
+				'src/**/*.js'
+			],
+			'rules': {
+				'quotes': [
+					'warn',
+					'single'
+				],
+				'semi': [
+					'error',
+					'always'
+				],
+				'no-unused-vars': 'warn'
+			}
+		}
+	]
+};
