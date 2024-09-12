5f995f46161dcbdeac66194ac2e9f7cad718f92d
Alex <email@gmail.com>
2024-09-12 10:55:22 +0700

    задание на использование событий на странице проекта - готовы

--- src/js/script.js
+++ src/js/script.js
@@ -27,25 +27,68 @@ const movieDB = {
     ]
 };
 
-// 1) удаление рекламных блоков
+// 
 document.querySelectorAll('.promo__adv img').forEach(img => img.remove());
 
-// 2) изменение жанра
+// 
 document.querySelector('.promo__genre').textContent = 'драма';
 
-// 3) изменение постера
+// 
 document.querySelector('.promo__bg').style.backgroundImage = 'url("img/bg.jpg")';
 
-// 4, 5) формирование списка фильмов
-let listFilms = '';
+// + 2, 5) обрезаем название фильма
+const listFilm = document.querySelector('.promo__interactive-list');
 
-movieDB.movies.sort();
-movieDB.movies.forEach((film, i) => {
-    listFilms += `
-        <li class="promo__interactive-item">${i + 1}) ${film}
-            <div class="delete"></div>
-        </li>
-    `;
+const renderFilm = () => {
+    let listFilms = '';
+
+    movieDB.movies.sort();
+    movieDB.movies.forEach((film, i) => movieDB.movies[i] = film.toLowerCase());
+
+    movieDB.movies.forEach((film, i) => {
+        if (film.length > 21) film = `${film.slice(0, 21)}...`;
+
+        listFilms += `
+            <li class="promo__interactive-item">${i + 1}) ${film}
+                <div class="delete"></div>
+            </li>
+        `;
+    });
+
+    listFilm.innerHTML = listFilms;
+};
+
+renderFilm();
+
+// 1, 4) добовление фильма
+const form = document.querySelector('form.add');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const input = form.querySelector('.adding__input');
+
+    if (input.value.trim()) {
+        if (form.querySelector('[type="checkbox"]').checked) console.log('Добавляем любимый фильм');
+
+        movieDB.movies.push(input.value);
+        renderFilm();
+        form.reset();
+    }
 });
 
-document.querySelector('.promo__interactive-list').innerHTML = listFilms;
+// 3) удаление фильма
+listFilm.addEventListener('click', (e) => {
+    const t = e.target;
+
+    if (t && t.classList.contains('delete')) {
+        const list = listFilm.querySelectorAll('.promo__interactive-item');
+
+        for (let i = 0; i < list.length; i++) {
+            if (list[i] == t.closest('.promo__interactive-item')) {
+                movieDB.movies.splice(i, 1);
+                renderFilm();
+            }
+        }
+    }
+});
